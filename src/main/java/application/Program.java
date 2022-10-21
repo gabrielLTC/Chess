1771c60bcaa1aabf015ea3c05bd4f31ad9fd8fdb
tltzgltc <gabriellannes@id.uff.br>
2022-10-20 23:31:04 -0300

    fix program and Position

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -3,11 +3,9 @@ package application;
 import boardgame.Position;
 
 public class Program {
-
     public static void main(String[] args) {
-        Position position  = new Position(3, 5);
 
-        position.toString();
-
+        Position position = new Position(3, 5);
+        System.out.println(position);
     }
 }
